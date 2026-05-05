dd38737eb7c5ae0c57b87edaabbf3ceaa5a379c3
Thiago <thiagoc@usp.br>
2026-05-05 13:40:21 -0300

    Conserta flake.nix e testa flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = {
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -21,6 +21,5 @@
                     '';
         };
       };
-  };
 
 }
