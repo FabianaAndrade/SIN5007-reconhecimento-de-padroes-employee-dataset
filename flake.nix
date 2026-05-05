29cf50ca08fc37441b4d4a0e4e028e4eb951c63e
Thiago <thiagoc@usp.br>
2026-05-05 13:28:26 -0300

    Adiciona flake para nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Ambiente para desenvolvimento de atividades do curso SIN5007";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+      {
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            uv
+            python315
+          ];
+
+          shellHook = ''
+                    echo "Shell para SIN5007 inicializada..."
+                    '';
+        };
+      };
+  };
+
+}
